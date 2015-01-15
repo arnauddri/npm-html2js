b910d25d21cf1fd7e51018846142e382689cb29b
arnauddri <arnaud.drizard@gmail.com>
2015-01-15 20:57:35 +0100

    mocha setup

---
+++ test/html2js.spec.js
@@ -0,0 +1,11 @@
+/* globals describe, it */
+'use strict';
+
+var fs = require('fs');
+
+var html2js = require('../src/index.js');
+
+describe('html2js', function() {
+  it('should work', function() {
+  });
+});

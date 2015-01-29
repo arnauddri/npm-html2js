c09c80cd044b377610bdb5436e501281af88ca6a
arnauddri <arnaud.drizard@gmail.com>
2015-01-29 11:25:28 +0100

    fix template styling

--- test/output/model.js
+++ test/output/model.js
@@ -1,21 +1,21 @@
-angular.module('template.js', []);
+angular.module('template.js', [])
   .run(['$templateCache', function($templateCache) {
-    .$templateCache.put('file1.tpl.jade',
+    $templateCache.put('file1.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from file1!</h1>\n' +
     '</div>')
-  .$templateCache.put('file2.tpl.jade',
+  $templateCache.put('file2.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from file2!</h1>\n' +
     '</div>')
-  .$templateCache.put('subfolder/subfile1.tpl.jade',
+  $templateCache.put('subfolder/subfile1.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from subfile1!</h1>\n' +
     '</div>')
-  .$templateCache.put('subfolder/subfile2.tpl.jade',
+  $templateCache.put('subfolder/subfile2.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from subfile2!</h1>\n' +

f12d27f1cb91c71d86c83f4695a41987d8d4c27a
Sepp Renfer <sepp.renfer@gmail.com>
2015-07-01 23:47:05 +0100

    added missing semicolon to templateCache.tmpl

--- test/output/model.js
+++ test/output/model.js
@@ -4,21 +4,21 @@ angular.module('template.js', [])
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from file1!</h1>\n' +
-    '</div>')
+    '</div>');
   $templateCache.put('file2.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from file2!</h1>\n' +
-    '</div>')
+    '</div>');
   $templateCache.put('subfolder/subfile1.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from subfile1!</h1>\n' +
-    '</div>')
+    '</div>');
   $templateCache.put('subfolder/subfile2.tpl.jade',
     '\n' +
     '<div>\n' +
     '  <h1>Hello World from subfile2!</h1>\n' +
-    '</div>')
+    '</div>');
 
   }]);
